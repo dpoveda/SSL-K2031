22dc5fcf6f354b18439d9d95cc18e5752e71c06f
dpoveda <57077319+dpoveda@users.noreply.github.com>
2022-01-23 21:34:21 -0300

    Update scanner.h
    Corregí cosas en los tres archivos

--- TP 1/scanner.h
+++ TP 1/scanner.h
@@ -1,13 +1,16 @@
-/*
-TOKENS
-FDT = fin de texto; SEP = separador; CAD = cadena
-*/
-typedef enum
-{
-    FDT,
-    SEP,
-    CAD
-} TOKEN;
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+/* Prototipos para el manejo del buffer */
+extern char buffer[200];
+extern int bufferIndex;
+
+void agregar_caracter(char); //Agrega caracter al buffer
+void limpiar_buffer();       //Limpia el buffer
+
+//Defino los tokens
+enum token {FDT, CAD, SEP};
+typedef enum token TOKEN;
 
 //Prototipo para función get_token
 TOKEN get_token(void);
+#endif // SCANNER_H_INCLUDED
